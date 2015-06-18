bcd13e51dd6dc856855ab37dc094b96c6de653c8
Martin Karrer <support@bmalum.com>
2015-06-18 19:03:07 +0200

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
 
+  get "about" => "static_pages#about", :as => "about"
+  get "help" => "static_pages#help", :as => "help"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
