c796c96f5b439bb85e90e5110e80419b72f3e15f
Martin Karrer <support@bmalum.com>
2015-07-19 17:17:43 +0200

    Content for the First Page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :channels
   resources :channels
   resources :channels
+
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
