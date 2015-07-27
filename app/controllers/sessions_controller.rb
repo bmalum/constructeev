87cf603a34c7736a53f084fb41ff33928f55737a
Martin Karrer <support@bmalum.com>
2015-07-27 14:45:51 +0200

    Created a Session Object

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    channel = Channel.authenticate(params[:sec_hash])
+  	if channel
+    	session[:channel_id] = channel.id
+    	redirect_to root_url, :notice => "Logged in!"
+  	else
+    	flash.now.alert = "Invalid email or password"
+    	render "new"
+  	end
+  end
+
+ def destroy
+  session[:channel_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
+ end
+
+end
