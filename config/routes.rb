ba4bb71e33cefb34a4b253457ddcad6c07bc7eb9
Martin Karrer <support@bmalum.com>
2015-07-27 14:46:32 +0200

    Session Routes Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -17,7 +19,15 @@ Rails.application.routes.draw do
 
   resources :feedbacks
 
+  resources :sessions
+
   root 'static_pages#home'
+
+  get    'login'   => 'sessions#new'
+  
+  post   'login'   => 'sessions#create'
+  
+  get 'logout'  => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
