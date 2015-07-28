5b468ed4d3d3f28f1c77a2fd7afd8e3a4297889b
Martin Karrer <support@bmalum.com>
2015-07-28 19:05:57 +0200

    Update Mailer for MailGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'bMalum',
+  :password => <%= ENV["SENDGRID_PASSWD"] %>,
+  :domain => 'constructeev.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,13 +42,13 @@ Rails.application.configure do
   #LogRage
   config.lograge.enabled = true
 
-  config.action_mailer.delivery_method = :sendmail
+  ##config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  ##config.action_mailer.perform_deliveries = true
+  ##config.action_mailer.raise_delivery_errors = true
+  ##config.action_mailer.default_options = {from: 'no-reply@example.com'}
 end
