db8cc99da88d6336e895abe4cc4b5d39a1b906b0
Martin Karrer <support@bmalum.com>
2015-08-30 11:18:58 +0200

    Initial Code for AngularJS and FirstPage.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,11 @@
 //= require_directory .
+
+var app = angular.module('Constructeev', []);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.Frontpage_Title = 'Create YOUR own Channel now!';
+
+  $scope.toggle = function() {
+    angular.element(sidebar).sidebar('setting', 'transition', 'scale down').sidebar('toggle')
+  }
+});
